b9c80030dba5ac3d818238149b1f9f34645d4486
Hiroshi Sugawara <5000164@users.noreply.github.com>
2017-02-15 01:58:56 +0900

    sin 関数を使ってアニメーションするようにする

--- core/src/main/scala/jp/_5000164/libgdx_shape/ShapeGame.scala
+++ core/src/main/scala/jp/_5000164/libgdx_shape/ShapeGame.scala
@@ -3,6 +3,7 @@ package jp._5000164.libgdx_shape
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer
 import com.badlogic.gdx.graphics.{GL20, OrthographicCamera}
 import com.badlogic.gdx.{Game, Gdx, Screen}
+import scala.math.sin
 
 class ShapeGame extends Game {
   override def create() {
@@ -14,6 +15,7 @@ class TitleScreen(game: ShapeGame) extends Screen {
   lazy val camera = new OrthographicCamera()
   camera.setToOrtho(false, 800, 480)
   lazy val shapeRenderer = new ShapeRenderer()
+  var colorSeed = 0.0
 
   override def render(delta: Float) {
     Gdx.gl.glClearColor(0, 0, 0, 1)
@@ -21,13 +23,17 @@ class TitleScreen(game: ShapeGame) extends Screen {
 
     camera.update()
 
+    val color = sin(colorSeed)
+
     shapeRenderer.setProjectionMatrix(camera.combined)
     shapeRenderer.begin(ShapeRenderer.ShapeType.Filled)
-    shapeRenderer.setColor(1, 1, 1, 1)
+    shapeRenderer.setColor(color.toFloat, color.toFloat, color.toFloat, 1)
     shapeRenderer.circle(340, 240, 20)
     shapeRenderer.rect(380, 220, 40, 40)
     shapeRenderer.triangle(440, 220, 480, 220, 460, 260)
     shapeRenderer.end()
+
+    colorSeed += 0.06
   }
 
 
