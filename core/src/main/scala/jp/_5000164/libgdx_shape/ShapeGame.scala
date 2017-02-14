3287caccd4b200226fd3885ebef89b0ca9c94f9e
Hiroshi Sugawara <5000164@users.noreply.github.com>
2017-02-15 02:26:38 +0900

    丸四角三角をそれぞれ異なる色でアニメーションさせる

--- core/src/main/scala/jp/_5000164/libgdx_shape/ShapeGame.scala
+++ core/src/main/scala/jp/_5000164/libgdx_shape/ShapeGame.scala
@@ -15,7 +15,9 @@ class TitleScreen(game: ShapeGame) extends Screen {
   lazy val camera = new OrthographicCamera()
   camera.setToOrtho(false, 800, 480)
   lazy val shapeRenderer = new ShapeRenderer()
-  var colorSeed = 0.0
+  var colorSeedC = 0.0
+  var colorSeedR = 0.0
+  var colorSeedT = 0.0
 
   override def render(delta: Float) {
     Gdx.gl.glClearColor(0, 0, 0, 1)
@@ -23,17 +25,31 @@ class TitleScreen(game: ShapeGame) extends Screen {
 
     camera.update()
 
-    val color = sin(colorSeed)
+    val colorCR = (sin(colorSeedC * 1) + 1) / 2
+    val colorCG = (sin(colorSeedC * 2) + 1) / 2
+    val colorCB = (sin(colorSeedC * 4) + 1) / 2
+
+    val colorRR = (sin(colorSeedR * 1) + 1) / 2
+    val colorRG = (sin(colorSeedR * 2) + 1) / 2
+    val colorRB = (sin(colorSeedR * 4) + 1) / 2
+
+    val colorTR = (sin(colorSeedT * 1) + 1) / 2
+    val colorTG = (sin(colorSeedT * 2) + 1) / 2
+    val colorTB = (sin(colorSeedT * 4) + 1) / 2
 
     shapeRenderer.setProjectionMatrix(camera.combined)
     shapeRenderer.begin(ShapeRenderer.ShapeType.Filled)
-    shapeRenderer.setColor(color.toFloat, color.toFloat, color.toFloat, 1)
+    shapeRenderer.setColor(colorCR.toFloat, colorCG.toFloat, colorCB.toFloat, 1)
     shapeRenderer.circle(340, 240, 20)
+    shapeRenderer.setColor(colorRR.toFloat, colorRG.toFloat, colorRB.toFloat, 1)
     shapeRenderer.rect(380, 220, 40, 40)
+    shapeRenderer.setColor(colorTR.toFloat, colorTG.toFloat, colorTB.toFloat, 1)
     shapeRenderer.triangle(440, 220, 480, 220, 460, 260)
     shapeRenderer.end()
 
-    colorSeed += 0.06
+    colorSeedC += 0.01
+    colorSeedR += 0.02
+    colorSeedT += 0.04
   }
 
 
