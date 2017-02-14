fb5158a6ab744166c7b25e5575b136da337d4d58
Hiroshi Sugawara <5000164@users.noreply.github.com>
2017-02-15 01:50:27 +0900

    文字を消して図形を描画してみる

--- core/src/main/scala/jp/_5000164/libgdx_shape/ShapeGame.scala
+++ core/src/main/scala/jp/_5000164/libgdx_shape/ShapeGame.scala
@@ -1,41 +1,33 @@
 package jp._5000164.libgdx_shape
 
-import com.badlogic.gdx.graphics.g2d.{BitmapFont, SpriteBatch}
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer
 import com.badlogic.gdx.graphics.{GL20, OrthographicCamera}
 import com.badlogic.gdx.{Game, Gdx, Screen}
 
 class ShapeGame extends Game {
-  lazy val batch = new SpriteBatch
-  lazy val font = new BitmapFont
-
   override def create() {
     setScreen(new TitleScreen(this))
-  }
-
-  override def render() {
-    super.render()
-  }
-
-  override def dispose() {
-    batch.dispose()
-    font.dispose()
   }
 }
 
 class TitleScreen(game: ShapeGame) extends Screen {
   lazy val camera = new OrthographicCamera()
   camera.setToOrtho(false, 800, 480)
+  lazy val shapeRenderer = new ShapeRenderer()
 
   override def render(delta: Float) {
     Gdx.gl.glClearColor(0, 0, 0, 1)
     Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT)
 
     camera.update()
-    game.batch.setProjectionMatrix(camera.combined)
 
-    game.batch.begin()
-    game.font.draw(game.batch, "Shape Game", 100, 100)
-    game.batch.end()
+    shapeRenderer.setProjectionMatrix(camera.combined)
+    shapeRenderer.begin(ShapeRenderer.ShapeType.Filled)
+    shapeRenderer.setColor(1, 1, 1, 1)
+    shapeRenderer.circle(340, 240, 20)
+    shapeRenderer.rect(380, 220, 40, 40)
+    shapeRenderer.triangle(440, 220, 480, 220, 460, 260)
+    shapeRenderer.end()
   }
 
 
